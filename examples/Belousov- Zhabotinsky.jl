52ae8287bedab60a3b4b16349e3b017bebcd6b58
Luigi Marangio <gigi@MacBook-Pro-di-Luigi.local>
2021-06-25 17:57:07 +0200

    new numeric error in BigFloat

--- examples/Belousov- Zhabotinsky.jl
+++ examples/Belousov- Zhabotinsky.jl
@@ -23,10 +23,12 @@ M = D*P
 
 v2, vinf = NoiseFourier.Fourier1D.rigorous_norm(M, k=500)
 
-#print(v2)
 print(vinf)
 
-function convert_to_float(M, n, m)
+
+##
+function convert_to_bigfloat(M, n, m)
+    setprecision(1000)
     Q = zeros(Complex{Float64},n,m)
     ze = 0
     zer = []
@@ -35,8 +37,8 @@ function convert_to_float(M, n, m)
     b = 0
     for i in 1:n
         for j in 1:m
-            a = Float64(real((M[i,j])))
-            b = Float64(imag((M[i,j])))
+            a = BigFloat(real((M[i,j])))
+            b = BigFloat(imag((M[i,j])))
             Q[i,j] = a + im*b
             if Q[i,j] == 0
                 ze = ze +1
@@ -71,40 +73,37 @@ function measure(x,v)
 end
 
 function Gamma(sigma,n)
-    return (exp((-n^2*sigma^2)/2))/(sigma*n*sqrt(2*pi))
+    setprecision(1000)
+    return BigFloat((exp((-n^2*sigma^2)/2))/(sigma*n*sqrt(2*pi)))
 end
 
-function appr_error(v)
+
+##
+
+setprecision(1000)
+Z = convert_to_bigfloat(M, Size, Size)
+v = zeros(Size)
+v = convert_to_bigfloat(v, Size, 1)
+v[2]=BigFloat(1.0)
+for i in 1:4000
+    v = Z * v
+end
+epsilo = BigFloat(norm(Z*v - v, 2))
+##
+
+function appr_error_new(v)
     Ci = 0
-    t = size(vinf)[1]
+    t = size(v)[1]
     for i in 1:t-1
-        Ci += vinf[i]
+        Ci += v[i]
     end
-    return (1/(1-vinf[t])) * Ci * (1 + Gamma(noise_size,Size) + 1/(noise_size*sqrt(2*pi))) * Gamma(noise_size, Size)
+    return (1/(1-v[t])) * Ci * (1 + epsilo + Gamma(noise_size,Size) + 1/(noise_size*sqrt(2*pi))) * Gamma(noise_size, Size)
 end
 
 function dft_error(Size, sigma, N)
     return Size * Gamma(sigma,N)
 end
 
-"
-function eigen_error(v)
-    Ci = 0
-    t = size(vinf)[1]
-   for i in 1:t-1
-        Ci += vinf[i]
-    end
-    return (1/(1-vinf[t])) * Ci *eps()
-end
-"
-
-@info dft_error(Size, noise_size, 2048)
-@info appr_error(vinf)
-
-#@info eigen_error(vinf)
-
-Z = convert_to_float(M, Size, Size)
-v = eigvecs(Z)[:,Size]
 p = 50000
 x = range(0,1, length = p)
 y = zeros(p)
@@ -112,3 +111,10 @@ for i in 1:p
     y[i] = measure(x[i],v)
 end
 plot(x,y,ylims=(0,5),fmt = :png)#,xticks=25:5:75)
+
+
+@info dft_error(Size, noise_size, 2048)
+@info appr_error_new(vinf)
+
+##
+
